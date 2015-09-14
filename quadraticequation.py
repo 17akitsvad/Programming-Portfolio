cf017a19405e9b17357220893300feffe76e4e56
Vadym Akitsa <vadymakitsa@Alveyworld-29.local>
2015-09-14 09:45:30 -0600

    Python files

---
+++ quadraticequation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+a = int(raw_input("Enter value of a: "))
+b = int(raw_input("Enter value of a: "))
+c = int(raw_input("Enter value of c: "))
+d = b * b - 4 * a * c
+if d < 0:
+	print "ROOTS are imaginary"
+else:
+	root1 = (-b + math.sqrt(d)) / (2.0 * a)
+	root2 = (-b - math.sqrt(d)) / (2.0 * a)
+	print "Root 1 = ", root1
+	print "Root 2 = ", root2
